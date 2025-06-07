d3e5a5c3b1f4c921bd090f94913323222a399b33
Paul Beard <paulbread88@gmail.com>
2025-06-07 17:31:25 +0100

    trim the junk

--- plot sector over time.R
+++ plot sector over time.R
@@ -8,6 +8,8 @@ library(ggiraph)
 jobs_data_raw <- readr::read_csv("./wayback_job_stats_sectors.csv")
 retro_jobs_data_raw <- readr::read_csv("./legacy_wayback_job_stats_sectors.csv")
 
+plot_color <- "violet"
+
 # manually create a scrape of the latest job posting numbers
 latest_data <-
   tibble::tribble(
@@ -30,7 +32,6 @@ latest_data <-
   dplyr::mutate(date = as.Date("2025-06-07"), .before = 1)
 
 categories_to_other <- c("Banking and finance", "Health", "Hedge funds", "IT", "Investment")
-
 
 raw_fields_req <- c("date", "sector", "job_count")
 
@@ -59,23 +60,6 @@ jobs_data_clean <-
   dplyr::summarise(job_count = sum(job_count), .by = c("date", "sector"))
 
 
-p <-
-jobs_data_clean |> 
-  # thin out overly dense sections by only having 1 date point per 2 month window
-  dplyr::mutate(floored_date = lubridate::floor_date(date, unit = "bimonth")) |> 
-  dplyr::slice_head(by = c("floored_date", "sector")) |>
-  ggplot() +
-  aes(x = date, 
-      y = job_count, 
-      fill = forcats::fct_reorder2(sector, date, job_count) |> forcats::fct_rev(), 
-      data_id = sector) +
-  geom_area_interactive(position = "fill") +
-  scale_fill_discrete(name = "")
-
-
-library(ggbump)
-
-
 bump_plot_data <-
 jobs_data_clean |> 
   # thin out overly dense sections by only having 1 date point per 2 month window
@@ -86,70 +70,6 @@ jobs_data_clean |>
   dplyr::mutate(sector_rank = rank(job_count, ties.method = "first"), .by = year) |> 
   dplyr::mutate(sector = forcats::fct_reorder2(sector, year, job_count))
 
-bump_plot_data |> 
-  ggplot() +
-  aes(x = year, 
-      y = sector_rank, 
-      color = sector) +
-  geom_bump(size = 1) +
-  geom_text(
-    data = bump_plot_data |> dplyr::filter(year == 2025),
-    mapping = aes(label = sector),
-    hjust = 0,
-    nudge_x = 0.25,
-    size = 4
-  ) +
-  scale_color_discrete(name = "") +
-  scale_x_continuous(
-    limits = c(NA, 2032), 
-    breaks = seq(from=2013, 2025, by = 2),
-    labels = seq(from=2013, 2025, by = 2),
-    name = "") +
-  scale_y_continuous(name = "Rank") +
-  theme_bw() +
-  theme(
-    legend.position = "none",
-    plot.title = element_text(color = data_color, face = "bold", size = 20),
-    plot.subtitle = element_text(color = data_color, face = "bold", size = 10),
-    axis.text.x = element_text(angle = 45, hjust = 1),
-    axis.text.y = element_blank(),
-    axis.ticks = element_blank(),
-    panel.border = element_blank()
-  ) 
-
-
-
-bump_plot_data |> 
-  ggplot() +
-  aes(x = year, 
-      y = job_count, 
-      fill = forcats::fct_rev(sector)) +
-  geom_col(
-    position = "fill",
-    width = 0.9,
-    alpha = 1) +
-  scale_fill_discrete(name = "") +
-  scale_x_continuous(
-    expand = c(0, 0),
-    breaks = seq(from=2013, 2025, by = 1),
-    labels = seq(from=2013, 2025, by = 1),
-    name = "") +
-  scale_y_continuous(
-    expand = c(0, 0),
-    breaks = seq(from=0, 1, by = 0.2),
-    labels = scales::label_percent(accuracy = 1),
-    name = "Proprtion of Jobs Posted (%)") +
-  theme_bw() +
-  theme(
-    plot.title = element_text(color = data_color, face = "bold", size = 20),
-    plot.subtitle = element_text(color = data_color, face = "bold", size = 10),
-    axis.text.x = element_text(angle = 90, vjust = 0.5),
-    panel.grid.major.x  = element_blank(),
-    panel.grid.major.y = element_blank(),
-    panel.grid.minor.y  = element_blank(),
-    panel.grid.minor.x = element_blank(),
-    panel.border = element_blank()
-  ) 
 
 
 bump_plot_data |> 
@@ -170,13 +90,9 @@ bump_plot_data |>
   labs(caption = "** Note jobs posted can be assigned multiple sector types") + 
   theme_bw() +
   theme(
-    plot.title = element_text(color = data_color, face = "bold", size = 20),
-    plot.subtitle = element_text(color = data_color, face = "bold", size = 10),
+    plot.title = element_text(color = plot_color, face = "bold", size = 20),
+    strip.background = element_rect(fill = plot_color),
+    plot.subtitle = element_text(color = plot_color, face = "bold", size = 10),
     axis.text.x = element_text(angle = 90, vjust = 0.5),
-    # panel.grid.major.x  = element_blank(),
-    # panel.grid.major.y = element_blank(),
-    # panel.grid.minor.y  = element_blank(),
-    # panel.grid.minor.x = element_blank(),
-    # panel.border = element_blank(),
     legend.position = "none"
   ) 
